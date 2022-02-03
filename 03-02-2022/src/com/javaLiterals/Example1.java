a84d6a15eb75789b284c23f5c4505a683bdb803a
Namdev Patil <pythonsessions66@gmail.com>
2022-02-03 12:27:18 +0530

    com.javaLiterals package added into project 03-02-2022

---
+++ 03-02-2022/src/com/javaLiterals/Example1.java
@@ -0,0 +1,43 @@
+package com.javaLiterals;
+
+public class Example1 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		// Write a java program to learner information(id, name, age, address) using java literals
+		
+		int learnerId = 4949;
+		String learnerName = "Hema";
+		int learnerAge = 21;
+		float learnerMarks = 78.50f;
+		String learnerAddress = "Hyderabad";
+		boolean learnerResult = true;
+		char learnerGrade = 'A';
+				
+		
+		System.out.print("Learner Id: ");
+		System.out.println(learnerId);
+		
+		System.out.print("Learner Name: ");
+		System.out.println(learnerName);
+		
+		System.out.print("Learner Age: ");
+		System.out.println(learnerAge);
+		
+		System.out.print("Learner Marks: ");
+		System.out.println(learnerMarks);
+		
+		System.out.print("Learner Address: ");
+		System.out.println(learnerAddress);
+		
+		System.out.print("Learner Result(true means pass and false means fail): ");
+		System.out.println(learnerResult);
+		
+		System.out.print("Learner Grade: ");
+		System.out.println(learnerGrade);
+
+	}
+
+}
