b6ae382208f54c7f8933293bfdb445f98b9cfadb
Namdev Patil <pythonsessions66@gmail.com>
2022-02-03 12:10:09 +0530

    Example1.java file added into the project 03-02-2022

---
+++ 03-02-2022/src/com/javaComments/Example1.java
@@ -0,0 +1,30 @@
+
+//user defined package name => it is single line comment
+package com.javaComments;
+
+
+/*
+ * user defined class name, 
+ * 
+ * we can declare user defined class name with class keyword
+ * 
+ * Example1 is user defined class name.
+ */
+
+public class Example1
+{
+	
+	/**
+	 * 
+	 * main() method: every java program or application execution starts from main method.
+	 * 
+	 * System.out.println() => statement help us to print some data or result on the screen or console
+	 * 	 
+	 */
+	
+	public static void main(String[] args)
+	{		
+		System.out.println("Hello Java");
+	}
+
+}
